09aaf1703f7ca8b87d51befdb17715b212d7e042
Francisco A. C. Souza <francisceioseph@gmail.com>
2018-02-26 23:28:27 -0300

    add state and country to the DB

--- src/db/migrations/20180110173302-create-models-tables.js
+++ src/db/migrations/20180110173302-create-models-tables.js
@@ -11,18 +11,24 @@ module.exports = {
       .then(() => queryInterface.createTable(models.UserDocuments.tableName, models.UserDocuments.attributes))
       .then(() => queryInterface.createTable(models.UserHomeAddress.tableName, models.UserHomeAddress.attributes))
       .then(() => queryInterface.createTable(models.UserPersonalInfo.tableName, models.UserPersonalInfo.attributes))
-      .then(() => queryInterface.createTable(models.CompanyUser.tableName, models.CompanyUser.attributes));
+      .then(() => queryInterface.createTable(models.CompanyUser.tableName, models.CompanyUser.attributes))
+      .then(() => queryInterface.createTable(models.City.tableName, models.City.attributes))
+      .then(() => queryInterface.createTable(models.State.tableName, models.State.attributes))
+      .then(() => queryInterface.createTable(models.Country.tableName, models.Country.attributes));
   },
   down: (queryInterface) => {
     queryInterface
       .dropTable(models.Email.tableName)
-      .then(() => queryInterface.dropTable(models.Company.tableName))
       .then(() => queryInterface.dropTable(models.Telephone.tableName))
       .then(() => queryInterface.dropTable(models.UserBankAccount.tableName))
       .then(() => queryInterface.dropTable(models.UserDocuments.tableName))
       .then(() => queryInterface.dropTable(models.UserHomeAddress.tableName))
       .then(() => queryInterface.dropTable(models.UserPersonalInfo.tableName))
+      .then(() => queryInterface.dropTable(models.Company.tableName))
       .then(() => queryInterface.dropTable(models.CompanyUser.tableName))
-      .then(() => queryInterface.dropTable(models.User.tableName));
+      .then(() => queryInterface.dropTable(models.User.tableName))
+      .then(() => queryInterface.dropTable(models.City.tableName))
+      .then(() => queryInterface.dropTable(models.Country.tableName))
+      .then(() => queryInterface.dropTable(models.State.tableName));
   },
 };
