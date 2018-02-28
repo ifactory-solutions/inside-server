d73f595fdad79687541600fe3736afe36b4600a3
Francisco Souza <Francisco.Souza@adtalem.com>
2018-02-28 15:56:28 -0300

    feat(states_cities): add routes to locations

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import loginRoute from './login';
 import companyUserRouter from './companyUser';
+import locationRouter from './locations';
+
+const routesManagers = [loginRoute, companyUserRouter, locationRouter];
 
 const loadRoutes = (router) => {
-  loginRoute(router);
-  companyUserRouter(router);
+  routesManagers.forEach(routeManager => routeManager(router));
 };
 
 export default loadRoutes;
