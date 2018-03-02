fba0e0056fbaaefcbb3d2cedf7656eb6683173d7
Francisco A. C. Souza <francisceioseph@gmail.com>
2018-03-01 22:29:03 -0300

    feat(employee): improve employee model

--- src/db/models/userHomeAddress.js
+++ src/db/models/userHomeAddress.js
@@ -2,10 +2,10 @@ module.exports = (sequelize, DataTypes) => {
   const UserHomeAddress = sequelize.define(
     'UserHomeAddress',
     {
-      line1: { allowNull: false, type: DataTypes.STRING },
-      line2: { allowNull: false, type: DataTypes.INTEGER },
-      line3: { allowNull: true, type: DataTypes.STRING },
-      line4: { allowNull: false, type: DataTypes.STRING },
+      streetAddress: { allowNull: false, type: DataTypes.STRING },
+      number: { allowNull: false, type: DataTypes.INTEGER },
+      apNumber: { allowNull: true, type: DataTypes.STRING },
+      neighborhood: { allowNull: false, type: DataTypes.STRING },
       zipcode: { allowNull: false, type: DataTypes.STRING },
       city: { allowNull: false, type: DataTypes.STRING },
       state: { allowNull: false, type: DataTypes.STRING },
