929d6f47edb3a2ff582e1e08f621a8615479b3c3
Francisco A. C. Souza <francisceioseph@gmail.com>
2018-03-16 16:31:59 -0300

    feat(permissions): add permissions/roles models

---
+++ src/db/models/permission.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+    const Permission = sequelize.define(
+        'Permission',
+        {
+            name: { allowNull: false, type: DataTypes.STRING },
+            context: { allowNull: false, type: DataTypes.STRING },
+            action: { allowNull: false, type: DataTypes.STRING },
+            resource: { allowNull: false, type: DataTypes.STRING },
+        },
+        {
+            tableName: 'Permissions',
+        }
+    );
+
+    Permission.associate = (models) => {
+        Permission.Role = Permission.belongsToMany(models.Role);
+    }
+
+    return Permission;
+}
