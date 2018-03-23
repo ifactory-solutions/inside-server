17a13d4c233f1826d0485c80a8b9f9d8f6a75495
Francisco A. C. Souza <francisceioseph@gmail.com>
2018-03-23 15:47:32 -0300

    feat(roles): Improve models for role system and add some seeders

--- src/db/models/userRole.js
+++ src/db/models/userRole.js
@@ -1,7 +1,14 @@
-module.exports = (sequelize) => {
+module.exports = (sequelize, DataTypes) => {
   const UserRole = sequelize.define(
     'UserRole',
-    {},
+    {
+      createdAt: {
+        allowNull: false,
+        defaultValue: new Date(),
+        type: DataTypes.DATE,
+      },
+      updatedAt: { allowNull: true, type: DataTypes.DATE },
+    },
     {
       tableName: 'UserRoles',
     },
